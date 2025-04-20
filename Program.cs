381bbeccb16ef4e73eb9a7b22ee80c943067c7ac
ZygoteCode <zygotecode@proton.me>
2025-04-20 22:09:28 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using VadSharp;
+
+public class Program
+{
+    private const int SAMPLE_RATE = 16000;
+    private const float THRESHOLD = 0.5f;
+    private const int MIN_SPEECH_DURATION_MS = 250;
+    private const float MAX_SPEECH_DURATION_SECONDS = float.PositiveInfinity;
+    private const int MIN_SILENCE_DURATION_MS = 100;
+    private const int SPEECH_PAD_MS = 30;
+
+    public static void Main()
+    {
+        Console.Title = "VadSharpExample | Made by https://github.com/GabryB03/";
+
+        string modelPath = Path.Combine(AppContext.BaseDirectory, "resources", "silero_vad.onnx");
+        string audioPath = Path.Combine(AppContext.BaseDirectory, "resources", "test.wav");
+
+        if (!File.Exists(modelPath))
+        {
+            Console.WriteLine($"Model file not found: {modelPath}");
+            return;
+        }
+
+        if (!File.Exists(audioPath))
+        {
+            Console.WriteLine($"Audio file not found: {audioPath}");
+            return;
+        }
+
+        VadDetector vadDetector = new VadDetector(modelPath, THRESHOLD, SAMPLE_RATE, MIN_SPEECH_DURATION_MS, MAX_SPEECH_DURATION_SECONDS, MIN_SILENCE_DURATION_MS, SPEECH_PAD_MS);
+        List<VadSpeechSegment> speechTimeList = vadDetector.GetSpeechSegmentList(audioPath);
+        StringBuilder sb = new StringBuilder();
+
+        foreach (VadSpeechSegment speechSegment in speechTimeList)
+        {
+            sb.AppendLine($"[-] Start second: {speechSegment.StartSecond.ToString().Replace(",", ".")}s, end second: {speechSegment.EndSecond.ToString().Replace(",", ".")}s");
+        }
+
+        Console.WriteLine(sb.ToString());
+        Console.ReadLine();
+    }
+}
